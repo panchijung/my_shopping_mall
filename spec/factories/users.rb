c7822becf9b668e871ada8264eaf03d50c08262e
Roy <pan.chi.roun@gmail.com>
2016-01-28 18:18:51 +0800

    admin in role

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,11 @@ FactoryGirl.define do
     email Faker::Internet.email
     password Faker::Internet.password
   end
+
+  factory :admin, class: User do
+    email Faker::Internet.email
+    password Faker::Internet.password(8)
+    role "admin"
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe User do
   # homework
-  context "admin user" do
-    it "can create a admin user" do
+  context "系統管理者" do
+    it "建立一個使用者為管理員" do
       user = create(:admin)
       expect(user.role).to eq "admin"
     end
